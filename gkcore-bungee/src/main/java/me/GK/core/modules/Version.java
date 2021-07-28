564f4dc9e4ca52e4927dbdaffa66a769913c24bd
RobotHanzo <nathanlee302@outlook.com>
2021-07-28 21:19:44 +0800

    support beta (2

--- gkcore-bungee/src/main/java/me/GK/core/modules/Version.java
+++ gkcore-bungee/src/main/java/me/GK/core/modules/Version.java
@@ -31,19 +31,19 @@ public class Version implements Comparable<Version> {
     	return compareTo(new Version(target))==0;
     }
     @Override public int compareTo(Version that) {
-        if(that == null)
+        if (that == null)
             return 1;
-        String[] thisParts = this.get().split("\\.");
-        String[] thatParts = that.get().split("\\.");
+        String[] thisParts = this.get().replaceAll("-beta[0-9]+", "").split("\\.");
+        String[] thatParts = that.get().replaceAll("-beta[0-9]+", "").split("\\.");
         int length = Math.max(thisParts.length, thatParts.length);
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             int thisPart = i < thisParts.length ?
-                Integer.parseInt(thisParts[i]) : 0;
+                    Integer.parseInt(thisParts[i]) : 0;
             int thatPart = i < thatParts.length ?
-                Integer.parseInt(thatParts[i]) : 0;
-            if(thisPart < thatPart)
+                    Integer.parseInt(thatParts[i]) : 0;
+            if (thisPart < thatPart)
                 return -1;
-            if(thisPart > thatPart)
+            if (thisPart > thatPart)
                 return 1;
         }
         return 0;
