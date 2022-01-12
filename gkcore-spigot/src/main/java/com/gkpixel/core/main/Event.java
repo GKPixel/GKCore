03edc93b39d43ddf2f6251ac65bbbb3a4b4db847
matthewchoiyiuhei <matthew.choi@dotdotfire.com>
2022-01-12 11:12:21 +0800

    Fixed levitation > 127 jump bug

--- gkcore-spigot/src/main/java/com/gkpixel/core/main/Event.java
+++ gkcore-spigot/src/main/java/com/gkpixel/core/main/Event.java
@@ -1,16 +1,24 @@
 package com.gkpixel.core.main;
 
+import com.gkpixel.core.GKCore;
 import com.gkpixel.core.containers.GKPlayer;
 import com.gkpixel.core.managers.GKPlayerManager;
 import com.gkpixel.core.managers.offlinecommand.OfflineCommandManager;
 import com.gkpixel.core.modules.GKPlayerDatabase;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import java.util.UUID;
 
@@ -59,6 +67,28 @@ public class Event implements Listener {
         if (GKP == null) return;
         if (GKP.inputListener.checkInput(msg)) {
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onEntityHurt(EntityDamageByEntityEvent event){
+        Entity entity = event.getEntity();
+        if(entity instanceof Player){
+            Player player = (Player)entity;
+            PotionEffect effect = player.getPotionEffect(PotionEffectType.LEVITATION);
+
+            if(effect.getAmplifier() > 127){
+                //slow falling
+                double originalValue = player.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE).getValue();
+                player.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE).setBaseValue(1.0);
+                new BukkitRunnable(){
+
+                    @Override
+                    public void run() {
+                        player.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE).setBaseValue(originalValue);
+                    }
+                }.runTaskLater(GKCore.instance, 1);
+            }
         }
     }
 }
