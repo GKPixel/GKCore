fd68c9f2d8a33343ba9651a9967a87ba3e0c880c
matthewchoiyiuhei <matthew.choi@dotdotfire.com>
2022-02-13 22:35:54 +0800

    Fixed display name and lore are uneditable in 1.16.5

--- gkcore-spigot/src/main/java/com/gkpixel/core/modules/ConfigurationSerializableAdapter.java
+++ gkcore-spigot/src/main/java/com/gkpixel/core/modules/ConfigurationSerializableAdapter.java
@@ -8,12 +8,14 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 
 import java.lang.reflect.Type;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 
 /**
@@ -36,15 +38,16 @@ public class ConfigurationSerializableAdapter implements JsonSerializer<Configur
             JsonDeserializationContext context) throws JsonParseException {
         final Map<String, Object> map = new LinkedHashMap<>();
 
+        Object result = null;
         for (Map.Entry<String, JsonElement> entry : json.getAsJsonObject().entrySet()) {
             final JsonElement value = entry.getValue();
             final String name = entry.getKey();
-
             if (value.isJsonObject() && value.getAsJsonObject().has(ConfigurationSerialization.SERIALIZED_TYPE_KEY)) {
-                map.put(name, this.deserialize(value, value.getClass(), context));
+                result = this.deserialize(value, value.getClass(), context);
             } else {
-                map.put(name, context.deserialize(value, Object.class));
+                result = context.deserialize(value, Object.class);
             }
+            map.put(name, result);
         }
         //Bukkit.getPlayer("hiIamG7").sendMessage("deserialized: "+ConfigurationSerialization.deserializeObject(map));
         try {
@@ -62,7 +65,26 @@ public class ConfigurationSerializableAdapter implements JsonSerializer<Configur
             JsonSerializationContext context) {
         final Map<String, Object> map = new LinkedHashMap<>();
         map.put(ConfigurationSerialization.SERIALIZED_TYPE_KEY, ConfigurationSerialization.getAlias(src.getClass()));
-        map.putAll(src.serialize());
+
+        Map<String, Object> serialized = new HashMap<>(src.serialize());
+        if(serialized.containsKey("display-name")){
+            String displayName = serialized.get("display-name").toString();
+            BaseComponent[] components = ComponentSerializer.parse(displayName);
+            String text = BaseComponent.toLegacyText(components);
+            serialized.put("display-name", (Object) text);
+        }
+        if(serialized.containsKey("lore")){
+            List<String> list = new ArrayList<String>((List<String>) serialized.get("lore"));
+            for(int i = 0 ; i < list.size() ; i++){
+                String line = list.get(i);
+                BaseComponent[] components = ComponentSerializer.parse(line);
+                String text = BaseComponent.toLegacyText(components);
+                list.set(i, text);
+            }
+            serialized.put("lore", (Object) list);
+        }
+        map.putAll(serialized);
+
         return context.serialize(map, objectStringMapType);
     }
 }
